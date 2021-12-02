eab8c127251e30864f3695979dc148968ed6aa42
Rodrigo Mady da Silva <rodrigo.mady@gmail.com>
2021-12-02 09:42:23 -0300

    [ADD] Add level 1 with activity 1 and 2

---
+++ level1/exercise1.js
@@ -0,0 +1,32 @@
+/**
+ * BairesDev NodeJS Bootcamp level 1.
+ * Exercise 1.
+ * Build your first NodeJS program.
+ * 
+ * @author  Rodrigo Mady
+ * @project NodeJS Bootcamp
+ * 
+ */
+
+// You should create a variable to hold your name, and one to hold your mentor’s name.
+let author = "Rodrigo Mady";
+let mentor = "Jorge Diaz";
+
+// You should compare both.
+let strcompare = author == mentor;  // Value assertion compare.
+
+// And print the result to console.
+console.log(strcompare);
+
+// On the same program add a variable to hold the integer 1 and another to hold the string ‘1’.
+let int = 1;
+let str = "1";
+
+// Compare both.
+let strintcompare1 = int ==  str; // Just value.
+let strintcompare2 = int === str; // Value and type assertions.
+
+// And print the result to console.
+console.log(strintcompare1);
+console.log(strintcompare2);
+
---
+++ level1/exercise2.js
@@ -0,0 +1,23 @@
+/**
+ * BairesDev NodeJS Bootcamp level 1.
+ * Exercise 2.
+ * Create 5 variables of different types.
+ * 
+ * @author  Rodrigo Mady
+ * @project NodeJS Bootcamp
+ * 
+ */
+
+// Create 5 variables of different types.
+let str   = "Rodrigo Mady";
+let int   = 54321;
+let bool  = false;
+let arr   = ['bar', 'foo', 7];
+let und   = undefined;
+
+// And for each one print to console its type. 
+console.log(typeof(str));
+console.log(typeof(int));
+console.log(typeof(bool));
+console.log(typeof(arr));
+console.log(typeof(und));
